c88761b6322f70f21c55c17e2471387fc6d34678
Guy Israeli <guy.israeli@gmail.com>
2014-07-20 12:38:58 +0300

    v 0.9.12

--- app/assets/javascripts/jquery.transit.js
+++ app/assets/javascripts/jquery.transit.js
@@ -22,7 +22,7 @@
 }(this, function($) {
 
   $.transit = {
-    version: "0.9.11",
+    version: "0.9.12",
 
     // Map of $.css() keys to values for 'transitionProperty'.
     // See https://developer.mozilla.org/en/CSS/CSS_transitions#Properties_that_can_be_animated
@@ -659,7 +659,7 @@
         if (i > 0) {
           this.style[support.transition] = transitionValue;
         }
-        $(this).css(properties);
+        $(this).css(theseProperties);
       });
     };
 
